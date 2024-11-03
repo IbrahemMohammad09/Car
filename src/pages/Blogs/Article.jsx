a2850ad833b75899c66700b65463bf415a37f96b
IbrahemMohammad09 <137792138+IbrahemMohammad09@users.noreply.github.com>
2024-11-04 00:17:43 +0300

    add article section

--- src/pages/Blogs/Article.jsx
+++ src/pages/Blogs/Article.jsx
@@ -7,58 +7,89 @@ import SideLink from '../../component/SharedComponents/sideLink/sideLink';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const carsHero = [
-    { title: 'Sport', icon: <FaCar/>, url:"/search" },
-    { title: 'Luxury', icon: <FaCar/>, url:"/search" },
-    { title: 'Family', icon: <FaCar/>, url:"/search" },
-    { title: 'Economy', icon: <FaCar/>, url:"/search" },
-    { title: 'Convertible', icon: <FaCar/>, url:"/search" }
+    { title: 'Sport', icon: <FaCar/>, url: "/search" },
+    { title: 'Luxury', icon: <FaCar/>, url: "/search" },
+    { title: 'Family', icon: <FaCar/>, url: "/search" },
+    { title: 'Economy', icon: <FaCar/>, url: "/search" },
+    { title: 'Convertible', icon: <FaCar/>, url: "/search" }
 ];
 
 const Article = () => {
-    const language = useLanguageContext();
-    const [article, setArticle] = useState(null);
+    const { language } = useLanguageContext();
+    const [t, i18n] = useTranslation();
+
+    const [article, setArticle] = useState();
+    
+    const [articlesAr, setArticlesAr] = useState();
+    const [articlesEn, setArticlesEn] = useState();
     const [error, setError] = useState(false);
     const { pathname } = useLocation();
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get("https://seomei.pythonanywhere.com/api/articles/")
             .then((response) => {
                 if (response.data.success) {
                     const data = response.data.data;
-                    
                     // استخراج `id` من `pathname`
                     const id = parseInt(pathname.replace("/blogs/", ""), 10);
 
                     // العثور على المقال الذي يطابق `id`
                     const selectedArticle = data.find((item) => item.pk === id);
 
-                    if (selectedArticle) {
-                        // اختيار الحقول حسب اللغة
-                        const articleData = {
+                    // اختيار الحقول حسب اللغة
+                    if (selectedArticle){
+                        // اختيار الحقول حسب اللغة للعربية والإنجليزية
+                        const arArticle = {
                             pk: selectedArticle.pk,
-                            header: language === 'AR' ? selectedArticle.header_ar : selectedArticle.header_en,
-                            summary: language === 'AR' ? selectedArticle.summary_ar : selectedArticle.summary_en,
-                            description: language === 'AR' ? selectedArticle.description_ar : selectedArticle.description_en,
+                            summary: selectedArticle.summary_ar,
+                            description: selectedArticle.description_ar,
                             photo: selectedArticle.photo,
                             link: selectedArticle.link,
-                            linkTitle: language === 'AR' ? selectedArticle.link_title_ar : selectedArticle.link_title_en
+                            linkTitle: selectedArticle.link_title_ar,
                         };
 
-                        setArticle(articleData);
-                    } else {
-                        setError(true);
-                        navigate("/error")
-                        
+
+                        const enArticle = {
+                            pk: selectedArticle.pk,
+                            summary: selectedArticle.summary_en,
+                            description: selectedArticle.description_en,
+                            photo: selectedArticle.photo,
+                            link: selectedArticle.link,
+                            linkTitle: selectedArticle.link_title_en,
+                        };
+
+
+                        setArticlesAr(arArticle);
+                        setArticlesEn(enArticle);
+                        setArticle(articlesEn);
+                    }else{
+                        navigate("/error");
                     }
+
                 } else {
                     setError(true);
+                    navigate("/error");
                 }
+
+
             })
+            
             .catch(() => setError(true));
-    }, [pathname, language]);
+    }, []);
+
+    useEffect(() => {   
+        if (language === "AR") {
+            setArticle(articlesAr);
+            console.log("Set to Arabic Articles");
+        } else {
+            setArticle(articlesEn);
+            console.log("Set to English Articles");
+        }
+      }, [language, articlesAr, articlesEn]);
 
     return (
         <div dir={language === 'AR' ? 'rtl' : 'ltr'}>
@@ -68,15 +99,39 @@ const Article = () => {
                 <p>Article not found.</p>
             ) : (
                 article && (
-                    <div>
-                        <h1>{article.header}</h1>
-                        <p>{article.summary}</p>
-                        <div dangerouslySetInnerHTML={{ __html: article.description }} />
-                        <img src={article.photo} alt="Article" />
-                        <a href={article.link}>{article.linkTitle}</a>
+                    <div className="mt-8 p-4 flex flex-col md:flex-row justify-between items-start rounded-lg overflow-hidden shadow-md">
+                        <div className="md:w-1/3 space-y-2">
+                            <img
+                                src={"https://seomei.pythonanywhere.com/" + article.photo}
+                                className="w-full h-auto object-cover shadow-lg rounded-md" // استخدم w-full لجعل الصورة تأخذ عرض الحاوية
+                                alt="Article"
+                            />
+                            <div className='flex flex-row'>
+                                <a
+                                    href='/blogs'
+                                    className="mr-8 mt-4 cursor-pointer border border-solid border-__brown bg-__brown text-white text-[1rem] font-bold leading-[25.8px] rounded-sm block no-underline duration-300 opacity-90 hover:opacity-100 w-fit py-2 px-8"
+                                >
+                                    {t("backtoart")}
+                                </a>
+                                <a
+                                    href={article.link} 
+                                    target="_blank"
+                                    className="mt-4 cursor-pointer border border-solid border-__brown bg-__brown text-white text-[1rem] font-bold leading-[25.8px] rounded-sm block no-underline duration-300 opacity-90 hover:opacity-100 w-fit py-2 px-8"
+                                >
+                                    {article.linkTitle}
+                                </a>
+                            </div>
+                            
+                        </div>
+                        <div className="md:w-2/3 flex justify-start">
+                            <div dangerouslySetInnerHTML={{ __html: article.description }} className="prose" />
+                        </div>
                     </div>
+
+
                 )
             )}
+                    
             <SideLink />
             <Footer />
         </div>
--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -6,8 +6,6 @@ import Footer from '../../component/SharedComponents/Footer/Footer';
 import SideLink from '../../component/SharedComponents/sideLink/sideLink';
 import MainTitle from '../../component/SharedComponents/MainTitle/MainTitle';
 import { useTranslation } from 'react-i18next';
-import img from '../../images/Home/unsplash_UF2nwAcD8Mo.png';
-import img2 from '../../images/Home/background@3x.png';
 import { motion } from "framer-motion";
 import { useLanguageContext } from '../../hooks/useLanguageContext';
 import axios from 'axios';
@@ -25,7 +23,7 @@ const Blogs = () => {
   const { language } = useLanguageContext();
   const [t, i18n] = useTranslation();
   const BlogTitle = t("BlogTitle");
-  const [selectedArticle, setSelectedArticle] = useState(null);
+
   const [articles, setArticles] = useState([]);
   const [articlesAr, setArticlesAr] = useState([]);
   const [articlesEn, setArticlesEn] = useState([]);
@@ -35,13 +33,7 @@ const Blogs = () => {
 
 
   
-  const handleShowDetails = (article) => {
-    setSelectedArticle(article);
-  };
 
-  const handleBackToList = () => {
-    setSelectedArticle(null);
-  };
 
   useEffect(() => {
     axios.get("https://seomei.pythonanywhere.com/api/articles/")
@@ -51,7 +43,6 @@ const Blogs = () => {
 
           const arArticles = data.map((item) => ({
             pk: item.pk,
-            header: item.header_ar,
             summary: item.summary_ar,
             description: item.description_ar,
             photo: item.photo,
@@ -61,7 +52,6 @@ const Blogs = () => {
 
           const enArticles = data.map((item) => ({
             pk: item.pk,
-            header: item.header_en,
             summary: item.summary_en,
             description: item.description_en,
             photo: item.photo,
@@ -78,18 +68,14 @@ const Blogs = () => {
       })
       .catch((error) => {
         setError("An error occurred while fetching data.");
-        console.error(error);
       });
   }, []);
 
   useEffect(() => {
     setArticles(language === 'AR' ? articlesAr : articlesEn);
     
-    if (selectedArticle) {
-      const updatedArticle = (language === 'AR' ? articlesAr : articlesEn).find(article => article.pk === selectedArticle.pk);
-      setSelectedArticle(updatedArticle);
-    }
-  }, [language, selectedArticle, articlesAr, articlesEn]);
+
+  }, [language, articlesAr, articlesEn]);
 
   return (
     <div dir={language === 'AR' ? 'rtl' : 'ltr'}>
@@ -99,46 +85,6 @@ const Blogs = () => {
         <div>
           <MainTitle title={BlogTitle} />
           <div className="container mx-auto p-4">
-            {selectedArticle ? (
-              <motion.div
-                initial={{ opacity: 0, x: 100 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.5, delay: 0.2 }}
-                viewport={{ once: false, amount: 0.5 }}
-              >
-                <div className="flex flex-col md:flex-row items-center md:items-start gap-8">
-                  <img
-                    src={`https://seomei.pythonanywhere.com/${selectedArticle.photo}`}
-                    alt={selectedArticle.header}
-                    className="w-full md:w-[300px] h-auto object-cover rounded-lg"
-                  />
-                  <div className="flex flex-col justify-center">
-                    <h1 className="text-__brown text-3xl font-semibold mb-2">{selectedArticle.header}</h1>
-                    {/* <p className="text-lg mt-2">{selectedArticle.description}</p> */}
-                    <div
-                      dangerouslySetInnerHTML={{__html: selectedArticle.description}}
-                      />
-                    <a
-                      href={selectedArticle.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="mt-[30px] cursor-pointer border-[1px] border-solid border-__brown bg-__brown text-white text-[1rem] font-bold leading-[25.8px] rounded-sm block no-underline duration-300 opacity-90 hover:opacity-100 w-fit py-[10px] px-[30px]"
-                    >
-                      {selectedArticle.linkTitle}
-                    </a>
-                    <button
-                      className="mt-[30px] cursor-pointer border-[1px] border-solid border-__brown bg-__brown text-white text-[1rem] font-bold leading-[25.8px] rounded-sm block no-underline duration-300 opacity-90 hover:opacity-100 w-fit py-[10px] px-[30px]"
-                      onClick={handleBackToList}
-                    >
-                      {t("BacktoArticles")}
-
-                      
-
-                    </button>
-                  </div>
-                </div>
-              </motion.div>
-            ) : (
               <div className="flex flex-col gap-8">
                 {articles.map((article) => (
                   <motion.div
@@ -151,8 +97,8 @@ const Blogs = () => {
                   >
                     <img src={"https://seomei.pythonanywhere.com/"+article.photo} alt={article.header} className="w-[350px] h-[350px] object-cover" />
                     <div className="p-4 w-full md:w-2/3">
-                      <h2 className="text-__brown text-3xl font-semibold mb-2">{article.header}</h2>
-                      <p className="text-secondary text-xl text-gray-600">{article.summary}</p>
+                      {/* <h2 className="text-__brown text-3xl font-semibold mb-2">{article.header}</h2> */}
+                      <p className="text-secondary text-3xl text-gray-600">{article.summary}</p>
                       <button
                         className="mt-[30px] cursor-pointer border-[1px] border-solid border-__brown bg-__brown text-white text-[1rem] font-bold leading-[25.8px] rounded-sm block no-underline duration-300 opacity-90 hover:opacity-100 w-fit py-[10px] px-[30px]"
                         onClick={() => {navigate("/blogs/"+article.pk)}}
@@ -163,7 +109,6 @@ const Blogs = () => {
                   </motion.div>
                 ))}
               </div>
-            )}
           </div>
         </div>
       ) : (
